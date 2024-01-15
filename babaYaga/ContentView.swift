2fb0bfd4ec2bac75913d4d283570ad955fd84d76
eric <1196573886f@gmail.com>
2024-01-15 12:17:45 +0800

    Initial Commit

---
+++ babaYaga/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  babaYaga
+//
+//  Created by Eric on 2024/1/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, this is babayaga. First apple app from Eric")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ babaYaga/babaYagaApp.swift
@@ -0,0 +1,17 @@
+//
+//  babaYagaApp.swift
+//  babaYaga
+//
+//  Created by Eric on 2024/1/15.
+//
+
+import SwiftUI
+
+@main
+struct babaYagaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
